c847b6bcf32c3e29a3b84da0aff2cc9ee4d2e252
Paul Reeves <preeves@splunk.com>
2022-05-25 15:58:31 -0700

    Added the rest of the function

--- S3-SQS-Lambda-Firehose-Resources/lambda.py
+++ S3-SQS-Lambda-Firehose-Resources/lambda.py
@@ -1,17 +1,19 @@
-import boto3, gzip, json, os, sys, shutil, tarfile
+import boto3, gzip, json, os, sys, shutil, re, dateutil.parser, time
 
 # AWS-related setup
 s3Client = boto3.client('s3')
 firehoseDeliverySreamName = os.environ['firehoseDeliverySreamName']
 firehoseClient = boto3.client('firehose', region_name=os.environ['AWS_REGION'])
+recordBatch = []
 
 # Splunk-related setup
 SPLUNK_INDEX = boto3.client('firehose', region_name=os.environ['SPLUNK_INDEX'])
-SPLUNK_TIME = boto3.client('firehose', region_name=os.environ['SPLUNK_TIME'])
+SPLUNK_TIME_PREFIX = boto3.client('firehose', region_name=os.environ['SPLUNK_TIME_PREFIX'])
+SPLUNK_TIME_FORMAT = boto3.client('firehose', region_name=os.environ['SPLUNK_TIME_FORMAT'])
 SPLUNK_SOURCETYPE = boto3.client('firehose', region_name=os.environ['SPLUNK_SOURCETYPE'])
 SPLUNK_SOURCE = boto3.client('firehose', region_name=os.environ['SPLUNK_SOURCE'])
 SPLUNK_HOST = boto3.client('firehose', region_name=os.environ['SPLUNK_HOST'])
-SPLUNK_JSON_KEY = boto3.client('firehose', region_name=os.environ['SPLUNK_JSON_KEY'])
+SPLUNK_JSON_FORMAT = boto3.client('firehose', region_name=os.environ['SPLUNK_JSON_FORMAT'])
 SPLUNK_CSV_IGNORE_FIRST_LINE = boto3.client('firehose', region_name=os.environ['SPLUNK_CSV_IGNORE_FIRST_LINE'])
 
 
@@ -68,6 +70,9 @@ def uncompressFile(path):
 				with open(uncompressedFilePath, 'wb') as f_out:
 					shutil.copyfileobj(f_in, f_out)
 
+			# Remove the uncompressed file
+			os.remove(uncompressedFilePath)
+
 			return(path)
 
 	except:
@@ -75,20 +80,10 @@ def uncompressFile(path):
 
 	return(path)
 
-# Process the log file by reading it to memory and sending to Firehose
-def processLogFile(path):
 
-	# Try to read file contents into memory
-	try:
-		with open(path, 'r') as f:
-			events = f.read()
-	except:
-		return("Unable to read file contents into memory")
+# Split events into a list. Additional file extensions should be added here.
+def splitEvents(events, extension):
 
-	# Set extension 
-	extension = path.split(".")[-1]
-
-	# Split events into a list. Additional file extensions should be added here.
 	if (extension == "csv" or extension == "log"):
 		splitEvents = events.split("\n")
 		events.clear()
@@ -97,6 +92,39 @@ def processLogFile(path):
 		if (SPLUNK_JSON_FORMAT == "eventsInRecords"):
 			splitEvents = json.loads(events)["Records"]
 			events.clear()
+
+
+# Set timestamp on event
+def getTimestamp(event):
+
+	# For ISO8601 (%Y-%m-%dT%H-%M-%S.%fZ)
+	if (SPLUNK_TIME_FORMAT = "ISO8601"):
+		iso8601Timestamp = re.search("" + SPLUNK_TIME_PREFIX + ".{1,5}(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z)", str(splitEvent)).group(1) #fix eventTime
+		return(dateutil.parser.parse(iso8601Timestamp).timestamp())
+	# If not standard, set to current time
+	else:
+		return(time.time())
+
+# Buffer and send events to Firehose
+def sendEventsToFirehose(event, final):
+
+	# Add current event ot recordBatch
+	if (len(event) > 0): # This will be 0 if it's a final call to clear the buffer
+		recordBatch.append({"Data": event})
+
+	try:
+
+		# If there are more than 200 records or 2MB in the sending queue, send the event to Splunk and clear the queue
+		if (len(recordBatch) > 200 or (sys.getsizeof(recordBatch) > 2000000 )):
+			firehoseClient.put_record_batch(DeliveryStreamName=firehoseDeliverySreamName, Records=recordBatch)
+			recordBatch.clear()
+		elif (final == True):
+			firehoseClient.put_record_batch(DeliveryStreamName=firehoseDeliverySreamName, Records=recordBatch)
+			recordBatch.clear()
+	except:
+		return("Unable to send file to Firehose")
+
+	return("Sent to Firehose")
 
 
 # Default Lambda handler
@@ -129,5 +157,42 @@ def handler(event, context):
 			print("Unable to uncompress file s3://" + objectInfo["bucket"] + "/" + objectInfo["key"])
 			continue
 
-		# Send file info to be processed
-		processesResult = processLogFile(uncompressResult)
+		# Try to read the file contents into memory
+		try:
+			with open(path, 'r') as f:
+				events = f.read()
+		except:
+			print("Unable to read file contents into memory")
+			continue
+
+		# Set extension 
+		extension = path.split(".")[-1]
+
+		# Split events
+		splitEvents = splitEvents(events, extension)
+
+		# Loop through split events
+		for splitEvent in splitEvents:
+
+			# Get timetamp
+			timestamp = getTimestamp(event)
+
+			# Construct event to send to Splunk
+			event = { "time": timestamp, "host": SPLUNK_HOST, "source": SPLUNK_SOURCE, "sourcetype": SPLUNK_SOURCETYPE, "index": SPLUNK_INDEX, "event":  splitEvent }
+
+			# Buffer and send the evnets to Firehose
+			result = sendEventsToFirehose(str(event), False)
+
+			# Error logging
+			if (result == "Unable to send to Firehose"):
+				print("Unable to send to Firehose: " + firehoseDeliverySreamName)
+
+		# Send the remaining events to Firehose, effectively clearing the buffered events in recordBatch
+		sendEventsToFirehose("", True)
+
+		# Delete the file to clear up space in /tmp to make room for the next one
+		os.remove(path)
+
+		# Logging
+		print("Processed file s3://" + objectInfo["bucket"] + "/" + objectInfo["key"])
+
