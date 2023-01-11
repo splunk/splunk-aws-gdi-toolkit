63fdeadba89bef94397e60cf22074c2804ea4fc8
Paul Reeves <preeves@splunk.com>
2023-01-11 11:13:06 -0700

    Added integration tests

--- CloudWatchMetrics-Firehose-Resources/test.py
+++ CloudWatchMetrics-Firehose-Resources/test.py
@@ -2,7 +2,7 @@ import unittest, os
 
 # Set environment variables
 os.environ['SPLUNK_SOURCE'] = "841154226728"
-os.environ['SPLUNK_EVENT_TYPE'] = "event"
+os.environ['SPLUNK_EVENT_TYPE'] = "metric" #"event"
 os.environ['SPLUNK_HOST'] = "841154226728"
 os.environ['SPLUNK_INDEX'] = "aws"
 
@@ -33,10 +33,28 @@ class CloudWatchMetrics_Firehose_Resources_Tests(unittest.TestCase):
 
 			self.assertEqual(lambda_module.parseEventAsEvent(testInput), testOutput[:-1])
 
+	def test_integration_event(self):
+
+		# Read in test inputs
+		test_input = []
+		with open('test_fixtures_integration_event_events.txt') as test_fixtures_integration_event_events:
+			test_input = test_fixtures_integration_event_events.readlines()
+
+		# Read in test outputs
+		test_output = []
+		with open('test_fixtures_integration_event_returnRecords.txt') as test_fixtures_integration_event_returnRecords:
+			test_output = test_fixtures_integration_event_returnRecords.readlines()
+
+		testRange = range(len(test_input))
+		for i in testRange:
+
+			testInput = eval(test_input[i])
+			testOutput = eval("{'records':" + test_output[i]+ "}")
+
+			self.assertEqual(lambda_module.handler(testInput, "none"), testOutput)
+
 
 	def test_parseEventAsMetric(self):
-
-		os.environ['SPLUNK_EVENT_TYPE'] = "metric"
 
 		# Read in test inputs
 		test_input = []
@@ -55,6 +73,27 @@ class CloudWatchMetrics_Firehose_Resources_Tests(unittest.TestCase):
 			testOutput = test_output[i]
 
 			self.assertEqual(lambda_module.parseEventAsMetric(testInput), testOutput[:-1])
+
+
+	def test_integration_metric(self):
+
+		# Read in test inputs
+		test_input = []
+		with open('test_fixtures_integration_metric_events.txt') as test_fixtures_integration_metric_events:
+			test_input = test_fixtures_integration_metric_events.readlines()
+
+		# Read in test outputs
+		test_output = []
+		with open('test_fixtures_integration_metric_returnRecords.txt') as test_fixtures_integration_metric_returnRecords:
+			test_output = test_fixtures_integration_metric_returnRecords.readlines()
+
+		testRange = range(len(test_input))
+		for i in testRange:
+
+			testInput = eval(test_input[i])
+			testOutput = eval("{'records': " + test_output[i]+ "}")
+
+			self.assertEqual(lambda_module.handler(testInput, "none"), testOutput)
 
 
 if __name__ == '__main__':
