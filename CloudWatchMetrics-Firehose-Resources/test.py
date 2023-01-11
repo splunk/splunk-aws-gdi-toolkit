f29a88817c288459a8c999cef32be33762a79242
Paul Reeves <preeves@splunk.com>
2023-01-11 10:02:12 -0700

    Added tests for parseEventAsMetric

--- CloudWatchMetrics-Firehose-Resources/test.py
+++ CloudWatchMetrics-Firehose-Resources/test.py
@@ -12,17 +12,18 @@ lambda_module = importlib.import_module('lambda')
 
 class CloudWatchMetrics_Firehose_Resources_Tests(unittest.TestCase):
 
+
 	def test_parseEventAsEvent(self):
 
 		# Read in test inputs
 		test_input = []
-		with open('test_fixtures_parseEventAsEvent_messages.txt') as test_fixtures_events:
-			test_input = test_fixtures_events.readlines()
+		with open('test_fixtures_parseEventAsEvent_messages.txt') as test_fixtures_parseEventAsEvent_messages:
+			test_input = test_fixtures_parseEventAsEvent_messages.readlines()
 
 		# Read in test outputs
 		test_output = []
-		with open('test_fixtures_parseEventAsEvent_returns.txt') as test_fixtures_returnRecords:
-			test_output = test_fixtures_returnRecords.readlines()
+		with open('test_fixtures_parseEventAsEvent_returns.txt') as test_fixtures_parseEventAsEvent_returns:
+			test_output = test_fixtures_parseEventAsEvent_returns.readlines()
 
 		testRange = range(len(test_input))
 		for i in testRange:
@@ -31,6 +32,29 @@ class CloudWatchMetrics_Firehose_Resources_Tests(unittest.TestCase):
 			testOutput = test_output[i]
 
 			self.assertEqual(lambda_module.parseEventAsEvent(testInput), testOutput[:-1])
+
+
+	def test_parseEventAsMetric(self):
+
+		os.environ['SPLUNK_EVENT_TYPE'] = "metric"
+
+		# Read in test inputs
+		test_input = []
+		with open('test_fixtures_parseEventAsMetric_messages.txt') as test_fixtures_parseEventAsMetric_messages:
+			test_input = test_fixtures_parseEventAsMetric_messages.readlines()
+
+		# Read in test outputs
+		test_output = []
+		with open('test_fixtures_parseEventAsMetric_returns.txt') as test_fixtures_parseEventAsMetric_returns:
+			test_output = test_fixtures_parseEventAsMetric_returns.readlines()
+
+		testRange = range(len(test_input))
+		for i in testRange:
+
+			testInput = test_input[i]
+			testOutput = test_output[i]
+
+			self.assertEqual(lambda_module.parseEventAsMetric(testInput), testOutput[:-1])
 
 
 if __name__ == '__main__':
